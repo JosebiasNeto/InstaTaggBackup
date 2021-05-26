f4e849b0aed142c7f0077ed61d72ff6d6af607e3
Josebias Neto <josebiasbsneto@gmail.com>
2021-05-25 22:28:18 -0300

    import camera in build dependences

--- app/src/main/java/com/example/instatagg/MainActivity.kt
+++ app/src/main/java/com/example/instatagg/MainActivity.kt
@@ -1,27 +1,24 @@
 package com.example.instatagg
 
-import android.content.Context
-import android.content.pm.PackageManager
-import android.hardware.Camera
-
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
-    private fun checkCameraHardware(context: Context): Boolean {
-        return context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)
+
+
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    abstract class CameraDevice : AutoCloseable {
+
+
     }
-    /** A safe way to get an instance of the Camera object. */
-    fun getCameraInstance(): Camera? {
-        return try {
-            Camera.open() // attempt to get a Camera instance
-        } catch (e: Exception) {
-            // Camera is not available (in use or does not exist)
-            null // returns null if camera is unavailable
-        }
-    }
+
 }
