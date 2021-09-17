82f5ee018db9d2c6e1a65f641b9d3db8bbb2a1cd
Josebias Neto <josebiasbsneto@gmail.com>
2021-09-17 10:57:41 -0300

    Implement insert Tagg

--- app/src/main/java/com/example/instatagg/presentation/ui/CreateTaggFragment.kt
+++ app/src/main/java/com/example/instatagg/presentation/ui/CreateTaggFragment.kt
@@ -4,9 +4,11 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.instatagg.R
 import com.example.instatagg.databinding.CreateTaggBinding
+import com.example.instatagg.domain.model.Tagg
 import com.example.instatagg.presentation.viewmodel.MainViewModel
 import com.github.dhaval2404.colorpicker.MaterialColorPickerDialog
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -28,7 +30,17 @@ class CreateTaggFragment : DialogFragment() {
             builder.setView(binding?.root)
                 .setPositiveButton(R.string.txt_create,
                     DialogInterface.OnClickListener { dialog, id ->
-
+                        if(binding?.etTaggName!!.text.isEmpty()){
+                            Toast.makeText(it,"Your tagg was not created, you need to enter a name!", Toast.LENGTH_LONG).show()
+                        } else {
+                            viewModel.insertTagg(
+                                Tagg(
+                                    null,
+                                    binding?.etTaggName!!.text.toString(),
+                                    binding?.btnChoseColor!!.currentHintTextColor
+                                )
+                            )
+                        }
                     })
                 .setNegativeButton(R.string.txt_cancel,
                     DialogInterface.OnClickListener { dialog, id ->
@@ -40,6 +52,7 @@ class CreateTaggFragment : DialogFragment() {
     }
     private fun changeColor(color: Int){
         _binding?.btnChoseColor?.setBackgroundColor(color)
+        _binding?.btnChoseColor?.setHintTextColor(color)
     }
     private fun choseColor(){
         activity?.let {
@@ -52,6 +65,7 @@ class CreateTaggFragment : DialogFragment() {
                 .setTickColorPerCard(false)
                 .show()
         }
+
     }
 
     override fun onDestroyView() {
