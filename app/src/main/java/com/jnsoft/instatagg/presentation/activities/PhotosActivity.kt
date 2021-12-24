89cc939c1e2926893041ea2d8b1dc6b132457aea
Josebias Neto <josebiasbsneto@gmail.com>
2021-12-23 21:13:34 -0300

    Implement import photos

--- app/src/main/java/com/jnsoft/instatagg/presentation/activities/PhotosActivity.kt
+++ app/src/main/java/com/jnsoft/instatagg/presentation/activities/PhotosActivity.kt
@@ -1,8 +1,14 @@
 package com.jnsoft.instatagg.presentation.activities
 
+import android.content.ContentUris
+import android.content.Context
 import android.content.Intent
+import android.database.Cursor
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.view.*
 import android.widget.CheckBox
@@ -27,7 +33,7 @@ import com.jnsoft.instatagg.presentation.viewmodel.PhotosViewModel
 import com.jnsoft.instatagg.utils.OnItemClickListener
 import com.jnsoft.instatagg.utils.addOnItemClickListener
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.io.File
+import java.io.*
 import java.util.*
 
 class PhotosActivity : AppCompatActivity() {
@@ -133,18 +139,12 @@ class PhotosActivity : AppCompatActivity() {
             ActivityResultContracts.StartActivityForResult())
         { result ->
             if(result.resultCode == RESULT_OK){
-                if (result.data != null){
-                    val uriPhoto: Uri = result.data!!.data!!
-                    val cursor = contentResolver.query(uriPhoto,
-                        arrayOf(MediaStore.Images.Media.DATA), null, null, null)
-                    cursor!!.moveToFirst()
-                    val cIndex = cursor.getColumnIndex(arrayOf(MediaStore.Images.Media.DATA)[0])
-                    val pathPhoto = cursor.getString(cIndex)
-                    val newPhoto = copyFile(File(pathPhoto), System.currentTimeMillis().toString())
-                    viewModel.insertPhoto(Photo(newPhoto, tagg, null),
-                        (newPhoto.toUri().toFile().length()/(1024*1024)).toInt())
+                if (result.data!!.data != null){
+                    val uriPhoto = result.data!!.data!!
+                    val filePhoto = copyFile(File(getPath(applicationContext, uriPhoto)), System.currentTimeMillis().toString() + ".jpg")
+                    viewModel.insertPhoto(Photo(filePhoto, tagg, null),
+                        (filePhoto.toUri().toFile().length()/(1024*1024)).toInt())
                 }
-            }
             else if (result.data!!.clipData != null){
                 val newPhotos = arrayListOf<Uri>()
                 val mClipData = result.data!!.clipData
@@ -152,14 +152,90 @@ class PhotosActivity : AppCompatActivity() {
                     newPhotos.add(mClipData.getItemAt(i).uri)
                 }
                 newPhotos.map {
-                    val newPhoto = copyFile(File(it.toString()), System.currentTimeMillis().toString())
+                    val newPhoto = copyFile(File(getPath(applicationContext, it)), System.currentTimeMillis().toString() + ".jpg")
                     viewModel.insertPhoto(Photo(newPhoto, tagg, null),
                         (newPhoto.toUri().toFile().length()/(1024*1024)).toInt())
                 }
             } else {
                 Toast.makeText(this, "No images picked", Toast.LENGTH_LONG).show()
-            }
+            }}
         }
+    }
+    fun getPath(context: Context, uri: Uri): String {
+        val isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).toTypedArray()
+                val type = split[0]
+                if ("primary".equals(type, ignoreCase = true)) {
+                    return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+                }
+            } else if (isDownloadsDocument(uri)) {
+                val id = DocumentsContract.getDocumentId(uri)
+                val contentUri = ContentUris.withAppendedId(
+                    Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(id)
+                )
+                return getDataColumn(context, contentUri, null, null)
+            } else if (isMediaDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).toTypedArray()
+                val type = split[0]
+                var contentUri: Uri? = null
+                if ("image" == type) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                } else if ("video" == type) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                } else if ("audio" == type) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                }
+                val selection = "_id=?"
+                val selectionArgs: Array<String?> = arrayOf(
+                    split[1])
+                return getDataColumn(context, contentUri, selection, selectionArgs)
+            }
+        } else if ("content".equals(uri.scheme, ignoreCase = true)) {
+            return getDataColumn(context, uri, null, null)
+        } else if ("file".equals(uri.scheme, ignoreCase = true)) {
+            return uri.path!!
+        }
+        return ""
+    }
+    fun getDataColumn(
+        context: Context, uri: Uri?, selection: String?,
+        selectionArgs: Array<String?>?
+    ): String {
+        var cursor: Cursor? = null
+        val column = "_data"
+        val projection = arrayOf(
+            column
+        )
+        try {
+            cursor = context.contentResolver.query(
+                uri!!, projection, selection, selectionArgs,
+                null
+            )
+            if (cursor != null && cursor.moveToFirst()) {
+                val column_index: Int = cursor.getColumnIndexOrThrow(column)
+                return cursor.getString(column_index)
+            }
+        } finally {
+            if (cursor != null) cursor.close()
+        }
+        return ""
+    }
+
+    fun isExternalStorageDocument(uri: Uri): Boolean {
+        return "com.android.externalstorage.documents" == uri.authority
+    }
+
+    fun isDownloadsDocument(uri: Uri): Boolean {
+        return "com.android.providers.downloads.documents" == uri.authority
+    }
+
+    fun isMediaDocument(uri: Uri): Boolean {
+        return "com.android.providers.media.documents" == uri.authority
     }
 
     fun changeBottomOptionsVisibility(){
@@ -219,7 +295,7 @@ class PhotosActivity : AppCompatActivity() {
         val tagg = intent.getParcelableExtra<Tagg>("tagg")!!
         when (item.itemId) {
             R.id.import_photos -> {
-                val galery = Intent(Intent.ACTION_GET_CONTENT)
+                val galery = Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 galery.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
                 galery.putExtra(Intent.EXTRA_LOCAL_ONLY, true)
                 galery.type = "image/*"
--- app/src/main/java/com/jnsoft/instatagg/utils/Constants.kt
+++ app/src/main/java/com/jnsoft/instatagg/utils/Constants.kt
@@ -6,5 +6,7 @@ object Constants {
     const val TAG = "CameraXBasic"
     const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
     const val REQUEST_CODE_PERMISSIONS = 123
-    val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA,
+                                       Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                       Manifest.permission.READ_EXTERNAL_STORAGE)
 }
