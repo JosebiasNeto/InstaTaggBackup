9a22ea35b76ec128720ad84c94dd32fdd41ae5cf
Josebias Neto <josebiasbsneto@gmail.com>
2022-01-09 20:36:01 -0300

    Fix problem with zoom in and zoom out imageview

--- app/src/main/java/com/jnsoft/instatagg/presentation/fragments/FullscreenPhotoFragment.kt
+++ app/src/main/java/com/jnsoft/instatagg/presentation/fragments/FullscreenPhotoFragment.kt
@@ -2,29 +2,25 @@ package com.jnsoft.instatagg.presentation.fragments
 
 import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ScaleGestureDetector
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.annotation.RequiresApi
 import androidx.core.net.toFile
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import com.davemorrissey.labs.subscaleview.ImageSource
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 import com.jnsoft.instatagg.databinding.FragmentFullscreenPhotoBinding
 import com.jnsoft.instatagg.domain.model.Photo
-import com.squareup.picasso.Picasso
-import java.lang.Float.max
-import java.lang.Float.min
 
 class FullscreenPhotoFragment() : Fragment() {
     private lateinit var binding: FragmentFullscreenPhotoBinding
     private lateinit var photo: Photo
     private lateinit var scaleGestureDetector: ScaleGestureDetector
     private var scaleFactor = 1.0f
-    private lateinit var imageView: ImageView
+    private lateinit var imageView: SubsamplingScaleImageView
 
     companion object {
         fun newInstance(photo: Photo): Fragment {
@@ -41,31 +37,13 @@ class FullscreenPhotoFragment() : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         photo = requireArguments().getParcelable<Photo>("photo")!!
         binding = FragmentFullscreenPhotoBinding.inflate(layoutInflater)
         val imageSize = getImageSize(photo)
-        Picasso.get().load(photo.path).noFade().resize(imageSize[0],imageSize[1])
-            .into(binding.ivFullscreanPhoto)
         imageView = binding.ivFullscreanPhoto
-        scaleGestureDetector = ScaleGestureDetector(context, ScaleListener())
-        binding.root.setOnTouchListener { view, motionEvent ->
-            scaleGestureDetector.onTouchEvent(motionEvent)
-            return@setOnTouchListener true
-        }
-
+        photo.path?.let { ImageSource.uri(it) }?.let { imageView.setImage(it) }
         return binding.root
-    }
-
-    private inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener(){
-        @RequiresApi(Build.VERSION_CODES.N)
-        override fun onScale(detector: ScaleGestureDetector?): Boolean {
-            scaleFactor *= scaleGestureDetector.scaleFactor
-            scaleFactor = max(1f, min(scaleFactor, 10.0f))
-            imageView.scaleX = scaleFactor
-            imageView.scaleY = scaleFactor
-            return true
-        }
     }
 
     private fun getImageSize(photo: Photo): ArrayList<Int>{
